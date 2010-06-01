283680474183286c917023e4d29e34571f27d2bd
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-06-01 20:05:36 +0000

    Added treeStatus

--- trunk/web-services/modeling_v2_client/src/mit/simulation/climate/client/model/impl/ClientTuple.java
+++ trunk/web-services/modeling_v2_client/src/mit/simulation/climate/client/model/impl/ClientTuple.java
@@ -1,14 +1,17 @@
 package mit.simulation.climate.client.model.impl;
 
 import mit.simulation.climate.client.Tuple;
+import mit.simulation.climate.client.TupleStatus;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.*;
 
 @XmlRootElement
 public class ClientTuple implements Tuple {
 
 	String[] values;
+    Map<Integer,TupleStatus> statuses = new LinkedHashMap<Integer,TupleStatus>();
 
 	public ClientTuple() {
 	}
@@ -20,6 +23,9 @@ public class ClientTuple implements Tuple {
 	public String[] getValues() {
        return values;
     }
+
+
+
 
     public String toString() {
        StringBuffer buf = new StringBuffer();
@@ -34,7 +40,22 @@ public class ClientTuple implements Tuple {
 		values = vals;
 	}
 
-	public static class Adapter extends XmlAdapter<ClientTuple, Tuple> {
+    @Override
+    public TupleStatus getStatus(int index) {
+        return statuses.get(index);
+    }
+
+
+    @Override
+    public List<TupleStatus> getAllStatuses() {
+        return new ArrayList<TupleStatus>(statuses.values());
+    }
+
+    public void setStatus(int i, TupleStatus status) {
+        statuses.put(i,status);
+    }
+
+    public static class Adapter extends XmlAdapter<ClientTuple, Tuple> {
 
 		@Override
 		public ClientTuple marshal(Tuple arg0) throws Exception {
--- trunk/web-services/modeling_v2_client/src/mit/simulation/climate/client/model/jaxb/ClientTupleListAdapter.java
+++ trunk/web-services/modeling_v2_client/src/mit/simulation/climate/client/model/jaxb/ClientTupleListAdapter.java
@@ -1,11 +1,12 @@
 package mit.simulation.climate.client.model.jaxb;
 
 import mit.simulation.climate.client.Tuple;
+import mit.simulation.climate.client.TupleStatus;
 import mit.simulation.climate.client.model.impl.ClientTuple;
+import org.apache.log4j.Logger;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -16,68 +17,76 @@ import java.util.regex.Pattern;
 public class ClientTupleListAdapter {
 
 
-	@XmlElement(name="data")
-	String data;
+    @XmlElement(name = "data")
+    String data;
 
+    private static Logger log = Logger.getLogger(ClientTupleListAdapter.class);
 
-	public ClientTupleListAdapter() {
+    public ClientTupleListAdapter() {
 
-	}
+    }
 
-	public ClientTupleListAdapter(List<Tuple> src) {
-		if (src == null) data = null;
-		else {
-		data = "[";
-		for (Tuple t:src) {
-			String sep="";
-			data+="[";
-			for (String n:t.getValues()) {
-				data+=(sep+ (n==null?"null":n.toString()));
-				sep=",";
-			}
-			data+="]";
-		}
-		data+="]";
-		}
-	}
+    public ClientTupleListAdapter(List<Tuple> src) {
+        if (src == null) data = null;
+        else {
+            data = "[";
+            for (Tuple t : src) {
+                String sep = "";
+                data += "[";
+                for (String n : t.getValues()) {
+                    data += (sep + (n == null ? "null" : n.toString()));
+                    sep = ",";
+                }
+                data += "]";
+            }
+            data += "]";
+        }
+    }
 
     public static List<Tuple> parse(String data) {
-        String stripped =  data.substring(1,data.length()-1);
+        String stripped = data.substring(1, data.length() - 1);
         Pattern p = Pattern.compile("\\[(.*?)\\]");
         Matcher m = p.matcher(stripped);
         List<Tuple> result = new ArrayList<Tuple>();
         while (m.find()) {
             String[] splited = m.group(1).split(",");
-            for (int i=0; i < splited.length; i++) {
-                try {
-                    splited[i] = URLDecoder.decode(splited[i], "UTF-8");
-                } catch (UnsupportedEncodingException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+            ClientTuple tuple = new ClientTuple();
+            for (int i = 0; i < splited.length; i++) {
+                TupleStatus status = TupleStatus.lookup(splited[i]);
+                if (status != null) {
+                    log.debug("Setting status of "+status+" on element "+i);
+                    tuple.setStatus(i, status);
+                    splited[i] = null;
+                } else {
+                    try {
+                        splited[i] = URLDecoder.decode(splited[i], "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 }
             }
-            result.add(new ClientTuple(splited));
+            tuple.setValues(splited);
+            result.add(tuple);
         }
         return result;
     }
 
 
-	public static class Adapter extends XmlAdapter<ClientTupleListAdapter,List<Tuple>> {
+    public static class Adapter extends XmlAdapter<ClientTupleListAdapter, List<Tuple>> {
 
-		@Override
-		public ClientTupleListAdapter marshal(List<Tuple> v) throws Exception {
-			return new ClientTupleListAdapter(v);
-		}
+        @Override
+        public ClientTupleListAdapter marshal(List<Tuple> v) throws Exception {
+            return new ClientTupleListAdapter(v);
+        }
 
-		@Override
-		public List<Tuple> unmarshal(ClientTupleListAdapter v) throws Exception {
-			return parse(v.data);
-		}
-
-
-	}
+        @Override
+        public List<Tuple> unmarshal(ClientTupleListAdapter v) throws Exception {
+            return parse(v.data);
+        }
 
 
-    
+    }
+
 
 }
