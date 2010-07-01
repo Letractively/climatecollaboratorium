e515651b20b15b1f3a3259c5ee8e84d53cc9e475
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-07-01 22:08:12 +0000

    Updated tests.

--- trunk/web-services/modeling_v2_client/test/mit/simulation/climate/client/UnmarshallingTest.java
+++ trunk/web-services/modeling_v2_client/test/mit/simulation/climate/client/UnmarshallingTest.java
@@ -1,9 +1,6 @@
 package mit.simulation.climate.client;
 
-import mit.simulation.climate.client.comm.ClientRepository;
-import mit.simulation.climate.client.comm.MetaDataNotFoundException;
-import mit.simulation.climate.client.comm.ModelNotFoundException;
-import mit.simulation.climate.client.comm.ScenarioNotFoundException;
+import mit.simulation.climate.client.comm.*;
 import mit.simulation.climate.client.model.impl.ClientSimulation;
 import mit.simulation.climate.client.model.jaxb.ResponseWrapper;
 import org.apache.log4j.Logger;
@@ -86,29 +83,7 @@ public class UnmarshallingTest {
     @Test
     public void testCompositeModelRun() throws IOException, ScenarioNotFoundException, ModelNotFoundException {
         ClientRepository repo = ClientRepository.instance("localhost", 8080);
-
-        Simulation sim = repo.getSimulation(760L);
-
-        Map<Long, Object> inputs = new HashMap<Long, Object>();
-        //developed
-        inputs.put(3027L, "2012");  //start year
-        inputs.put(3022L, "2013");  //target year
-        inputs.put(3028L, "-.5");   //target
-
-        //developing a
-        inputs.put(3024L, "2012");
-        inputs.put(3020L, "2013");
-        inputs.put(3029L, "-.5");
-
-        //developing b
-        inputs.put(3031L, "2012");
-        inputs.put(3026L, "2013");
-        inputs.put(3032L, "0");
-
-
-        inputs.put(3034L, "0.50");  //sequestration (afforestation)
-        inputs.put(3030L, "0.50");  //deforestation
-        Scenario scenario = repo.runModel(sim, inputs, 1L, true);
+        Scenario scenario = TestHelper.runCompositeOne(repo);
         log.info("Scenario: "+scenario.getName()+" id:"+scenario.getId());
         log.info(getScenarioString(scenario));
         Assert.assertEquals(EntityState.TEMPORARY,scenario.getState());
@@ -117,33 +92,7 @@ public class UnmarshallingTest {
        @Test
      public void testCompositeModelRun2() throws IOException, ScenarioNotFoundException, ModelNotFoundException, MetaDataNotFoundException {
         ClientRepository repo = ClientRepository.instance("localhost", 8080);
-        Simulation sim = repo.getSimulation(981L);
-
-        Map<String, Object> inputs = new HashMap<String, Object>();
-        //developed
-        inputs.put("Developed_Nations_Change_Start_Year_input2", "2012");  //start year
-        inputs.put("Developed_Nations_Change_Target_Year_input3", "2050");  //target year
-         inputs.put("US_EU_Emissions_Change_input0","100"); //US & EU
-         inputs.put("Other_Developed_Change_input1", "100");   //other developed change
-
-        //developing a
-        inputs.put("Developing_Nations_Start_Year_input6", "2012");
-        inputs.put("Developing_Nations_Target_Year_input7", "2013");
-        inputs.put("China_India_Emissions_Change_input4", "100"); //china, india
-        inputs.put("Other_Developing_Change_input5","100"); //other developing change 
-
-        //developing b
-        inputs.put("Other_Nations_Start_Year_input10","2012");
-        inputs.put("Other_Nations_Target_Year_input11","2050");
-        inputs.put("Bloc_A_Emissions_Change_input8", "100"); //bloc a
-        inputs.put("Bloc_B_Emissions_Change_input9", "100"); //bloc b
-
-
-        inputs.put("Target Sequestration", "0.50");  //sequestration (afforestation)
-        inputs.put("Global land use emissions change", "0.50");  //deforestation
-
-        inputs.put("Percent_Transfer_from_Developed_to_Developing_input0", "50"); //percent transfer from developed to developing
-        Scenario scenario = repo.runModelWithInputNames(sim, inputs, 1L, true);
+        Scenario scenario = TestHelper.runCompositeTwo(repo);
         log.info("Scenario: "+scenario.getName()+" id:"+scenario.getId());
         log.info(getScenarioString(scenario));
         Assert.assertEquals(EntityState.TEMPORARY,scenario.getState());
