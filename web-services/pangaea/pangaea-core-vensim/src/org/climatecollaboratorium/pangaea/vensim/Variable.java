56ddd4e964681e2faff171b562049c8668c886f6
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-07-16 02:07:17 +0000

    Update to support a composite variable.

--- web-services/pangaea/pangaea-core-vensim/src/org/climatecollaboratorium/pangaea/vensim/Variable.java
+++ web-services/pangaea/pangaea-core-vensim/src/org/climatecollaboratorium/pangaea/vensim/Variable.java
@@ -7,6 +7,7 @@ package org.climatecollaboratorium.pangaea.vensim;
  * Time: 5:14:33 PM
  * To change this template use File | Settings | File Templates.
  */
-public interface VensimVariable {
+public interface Variable {
     String getInternalName();
+    float modify(float input);
 }
