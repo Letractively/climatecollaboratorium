2b6f11f7a7ffde26e59af258344758d5fc0dce66
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-07-19 10:01:05 +0000

    Plans portlet with integrated simulation tag

--- branches/hydra-0.9/plugins/portlets/plans-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/SimulationBean.java
+++ branches/hydra-0.9/plugins/portlets/plans-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/SimulationBean.java
@@ -1,12 +1,22 @@
 package org.climatecollaboratorium.plans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.event.ActionEvent;
 
+import org.climatecollaboratorium.events.EventBus;
+import org.climatecollaboratorium.events.EventHandler;
+import org.climatecollaboratorium.events.HandlerRegistration;
+import org.climatecollaboratorium.facelets.simulations.ScenarioEditEvent;
+import org.climatecollaboratorium.facelets.simulations.ScenarioSavedEvent;
 import org.climatecollaboratorium.plans.activity.PlanActivityKeys;
 
 import com.ext.portlet.plans.model.PlanItem;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.social.service.SocialActivityLocalServiceUtil;
 
@@ -17,22 +27,44 @@ public class SimulationBean {
     private PlanBean planBean;
     private boolean editing;
     private ThemeDisplay td = Helper.getThemeDisplay();
+    private EventBus eventBus;
+    private List<HandlerRegistration> eventHandlers = new ArrayList<HandlerRegistration>();
+    private static Log _log = LogFactoryUtil.getLog(SimulationBean.class);
     
-    public SimulationBean(PlanItem plan, PlanBean planBean) {
+    public SimulationBean(final PlanItem plan, final PlanBean planBean, EventBus eventBus) {
         this.plan = plan;
         this.planBean = planBean;
-    }
-    
-    public void update(ActionEvent e) throws SystemException, PortalException {
-        if (Helper.isUserLoggedIn()) {
-            plan.setScenarioId(scenario, Helper.getLiferayUser().getUserId());
+        this.eventBus = eventBus;
+        
+        eventHandlers.add(eventBus.registerHandler(ScenarioSavedEvent.class, new EventHandler<ScenarioSavedEvent>() {
+            @Override
+            public void onEvent(ScenarioSavedEvent arg0) {
+                if (Helper.isUserLoggedIn()) {
+                    try {
+                        plan.setScenarioId(arg0.getScenario().getId(), Helper.getLiferayUser().getUserId());
 
-            SocialActivityLocalServiceUtil.addActivity(td.getUserId(), td.getScopeGroupId(),
-                    PlanItem.class.getName(), plan.getPlanId(), PlanActivityKeys.EDIT_SCENARIO.id(),null, 0);
-            planBean.refresh();
+                        SocialActivityLocalServiceUtil.addActivity(td.getUserId(), td.getScopeGroupId(),
+                                PlanItem.class.getName(), plan.getPlanId(), PlanActivityKeys.EDIT_SCENARIO.id(),null, 0);
+                        planBean.refresh();
+                    } catch (PortalException e) {
+                        _log.error("Can't save scenario in a plan", e);
+                    } catch (SystemException e) {
+                        _log.error("Can't save scenario in a plan", e);
+                    }
+                    
+                }
+            }
             
-        }
-        editing = false;
+        }));
+        
+        eventHandlers.add(eventBus.registerHandler(ScenarioEditEvent.class, new EventHandler<ScenarioEditEvent>() {
+
+            @Override
+            public void onEvent(ScenarioEditEvent event) {
+                    setEditing(event.isEditing());
+            }
+            
+        }));
     }
     
     public void cancel(ActionEvent e) throws SystemException, PortalException {
@@ -51,8 +83,18 @@ public class SimulationBean {
         return editing;
     }
     
+    public void setEditing(boolean editing) {
+        this.editing = editing;
+    }
+    
     public void edit(ActionEvent e) {
         editing = true;
+    }
+
+    public void cleanup() {
+        for (HandlerRegistration handlerRegistration: eventHandlers) {
+            handlerRegistration.unregister();
+        }
     }
 
 }
