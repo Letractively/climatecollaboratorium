84a37994284a566e0e615e533acc46c7a10bc40f
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-07-22 15:59:42 +0000

    Fix for models inputs

---
+++ plugins/portlets/models-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/models/support/ModelDisplayWrapper.java
@@ -0,0 +1,94 @@
+package org.climatecollaboratorium.models.support;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.climatecollaboratorium.models.SimulationBean;
+
+import mit.simulation.climate.client.Simulation;
+
+
+import com.ext.portlet.models.ui.ModelDisplay;
+import com.ext.portlet.models.ui.ModelInputDisplayItem;
+import com.ext.portlet.models.ui.ModelInputGroupDisplayItem;
+import com.ext.portlet.models.ui.ModelOutputDisplayItem;
+
+public class ModelDisplayWrapper {
+    private ModelDisplay wrapped;
+    private SimulationBean simulationBean;
+    private List<ModelInputDisplayItemWrapper> wrappedInputs = new ArrayList<ModelInputDisplayItemWrapper>();
+    private List<ModelInputGroupDisplayItemWrapper> wrappedTabs = new ArrayList<ModelInputGroupDisplayItemWrapper>();
+    
+    public ModelDisplayWrapper(ModelDisplay wrapped, SimulationBean simulationBean, Map<Long, Object> values) {
+        this.wrapped = wrapped;
+        this.simulationBean = simulationBean;
+        
+        for (ModelInputGroupDisplayItem item: wrapped.getTabs()) {
+            System.out.println("tab: " + item.getOriginalName());
+            ModelInputGroupDisplayItemWrapper itemWrapper = new ModelInputGroupDisplayItemWrapper(item, simulationBean, values);
+            wrappedTabs.add(itemWrapper);
+            wrappedInputs.addAll(itemWrapper.getAllItems());
+        }
+    }
+    
+    public List<ModelOutputDisplayItem> getOutputs() {
+        return wrapped.getOutputs();
+    }
+    
+    public List<ModelInputDisplayItemWrapper> getInputs() {
+        return wrappedInputs;
+    }
+
+    public ModelDisplay getWrapped() {
+        return wrapped;
+    }
+
+    public Map<Long, Object> getInputsValues() {
+        Map<Long, Object> inputsValues = new HashMap<Long, Object>();
+        for (ModelInputDisplayItemWrapper item: wrappedInputs) {
+            if (! (item instanceof ModelInputGroupDisplayItemWrapper)) {
+                inputsValues.put(item.getWrapped().getMetaData().getId(), item.getValue());
+            } else {
+                getInputsValues((ModelInputGroupDisplayItemWrapper) item, inputsValues);
+            }
+        }
+        return inputsValues;
+    }
+
+    public void getInputsValues(ModelInputGroupDisplayItemWrapper group, Map<Long, Object> inputsValues) {
+        for (ModelInputDisplayItemWrapper groupedItem: group.getDisplayItemsWrapped()) {
+            if (groupedItem instanceof ModelInputGroupDisplayItemWrapper) {
+                getInputsValues((ModelInputGroupDisplayItemWrapper) groupedItem, inputsValues);
+            }
+            else {
+                inputsValues.put(groupedItem.getWrapped().getMetaData().getId(), groupedItem.getValue());
+            }
+            
+        }
+    }
+    
+    public List<ModelInputGroupDisplayItemWrapper> getTabsWrapped() {
+        return wrappedTabs;
+    }
+    
+    public List<ModelInputGroupDisplayItem> getTabs() {
+        return wrapped.getTabs();
+    }
+    
+    public boolean hasTabs() {
+        return wrapped.getTabs().size() > 0;
+    }
+    
+    public List<ModelInputDisplayItem> getOryginalInputs() {
+        return wrapped.getInputs();
+    }
+    
+    public List<ModelInputDisplayItem> getNonTabs() {
+        return wrapped.getNonTabs();
+    }
+    
+
+}
