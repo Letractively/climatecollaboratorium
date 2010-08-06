b9ba38a4db6235db73eed4b1b7245489e8713263
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-08-06 08:27:04 +0000

    Preventing form validation on cancel.

--- plugins/portlets/contactform-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/contact/validators/EmailValidator.java
+++ plugins/portlets/contactform-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/contact/validators/EmailValidator.java
@@ -9,15 +9,15 @@ import javax.faces.validator.ValidatorException;
 public class EmailValidator implements Validator {
 
     @Override
-        public void validate(FacesContext facesContext, 
-                UIComponent uIComponent, Object object) throws ValidatorException {
-            if (!((String) object).matches("(\\w[-._\\w]*\\w@\\w[-._\\w]*\\w\\.\\w{2,3})")) {
-                FacesMessage message = new FacesMessage();
-                message.setDetail("Email not valid");
-                message.setSummary("Email not valid");
-                message.setSeverity(FacesMessage.SEVERITY_ERROR);
-                throw new ValidatorException(message);
-            }
-        }
+    public void validate(FacesContext facesContext, UIComponent uIComponent, Object object) throws ValidatorException {
 
+        
+        if (!((String) object).matches("(\\w[-._\\w]*\\w@\\w[-._\\w]*\\w\\.\\w{2,3})")) {
+            FacesMessage message = new FacesMessage();
+            message.setDetail("Email not valid");
+            message.setSummary("Email not valid");
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message);
+        }
+    }
 }
