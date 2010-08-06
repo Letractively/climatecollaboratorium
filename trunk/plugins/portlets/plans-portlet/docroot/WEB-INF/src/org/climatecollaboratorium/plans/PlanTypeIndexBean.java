ef5d66df3f155900d43fea82361a7f5de25dfd62
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-08-06 18:38:41 +0000

    test svn

---
+++ trunk/plugins/portlets/plans-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/PlanTypeIndexBean.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2010. M.I.T. All Rights Reserved
+ * Licensed under the MIT license. Please see http://www.opensource.org/licenses/mit-license.php
+ * or the license.txt file included in this distribution for the full text of the license.
+ */
+
+package org.climatecollaboratorium.plans;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jintrone
+ * Date: Aug 6, 2010
+ * Time: 2:18:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlanTypeIndexBean {
+}
