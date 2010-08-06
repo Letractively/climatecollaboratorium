be5ce7aea11e6ff33825eaf7304e4fa1de048da0
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-08-06 18:45:53 +0000

    Attempting to force a commit.

--- trunk/ext/ext-impl/src/com/ext/portlet/contests/model/impl/ContestPhaseImpl.java
+++ trunk/ext/ext-impl/src/com/ext/portlet/contests/model/impl/ContestPhaseImpl.java
@@ -1,10 +1,33 @@
 package com.ext.portlet.contests.model.impl;
 
+import com.ext.portlet.contests.model.Contest;
+import com.ext.portlet.contests.model.ContestStatus;
 import com.ext.portlet.contests.model.ContestPhase;
+import com.ext.portlet.contests.service.ContestLocalServiceUtil;
+import com.ext.portlet.plans.model.PlanItem;
+import com.ext.portlet.plans.service.PlanItemLocalServiceUtil;
+import com.liferay.portal.PortalException;
+import com.liferay.portal.SystemException;
+
+import java.util.Collections;
+import java.util.List;
 
 
 public class ContestPhaseImpl extends ContestPhaseModelImpl
     implements ContestPhase {
     public ContestPhaseImpl() {
+    }
+
+    public Contest getContest() throws SystemException, PortalException {
+        return ContestLocalServiceUtil.getContest(this.getContestPK());
+    }
+
+    public List<PlanItem> getPlans() {
+       return PlanItemLocalServiceUtil.getPlansInContestPhase(this.getContestPhasePK());
+    }
+
+    public ContestStatus getContestStatus() {
+        String status = getContestPhaseStatus();
+        return status == null?null:ContestStatus.valueOf(status);
     }
 }
