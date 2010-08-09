ca052b9d7a9aeef3e0816024ccbc76f9f73a04a1
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-08-09 19:06:25 +0000

    Increasing title size.

--- trunk/ext/ext-impl/src/com/ext/portlet/contests/service/impl/ContestPhaseLocalServiceImpl.java
+++ trunk/ext/ext-impl/src/com/ext/portlet/contests/service/impl/ContestPhaseLocalServiceImpl.java
@@ -1,8 +1,21 @@
 package com.ext.portlet.contests.service.impl;
 
+import com.ext.portlet.contests.model.Contest;
+import com.ext.portlet.contests.model.ContestPhase;
 import com.ext.portlet.contests.service.base.ContestPhaseLocalServiceBaseImpl;
+import com.liferay.portal.SystemException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContestPhaseLocalServiceImpl
     extends ContestPhaseLocalServiceBaseImpl {
+
+
+    public List<ContestPhase> getPhasesForContest(Contest contest) throws SystemException {
+        List<ContestPhase> result = new ArrayList<ContestPhase>();
+        return contestPhasePersistence.findByContestId(contest.getContestPK());
+    }
+
 }
