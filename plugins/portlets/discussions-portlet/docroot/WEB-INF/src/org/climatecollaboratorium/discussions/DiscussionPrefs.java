86c33a75d59f7d4dad490f15a1f73a6c50927178
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-08-16 19:52:31 +0000

    Adds standalone discussions

---
+++ plugins/portlets/discussions-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/discussions/DiscussionPrefs.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2010. M.I.T. All Rights Reserved
+ * Licensed under the MIT license. Please see http://www.opensource.org/licenses/mit-license.php
+ * or the license.txt file included in this distribution for the full text of the license.
+ */
+
+package org.climatecollaboratorium.discussions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jintrone
+ * Date: Aug 15, 2010
+ * Time: 10:37:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiscussionPrefs {
+
+    private Long categoryId = 701L;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+    
+}
