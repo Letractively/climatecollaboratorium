a0a2574158497cc274e753637668bdc90fd740af
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-09-11 23:42:19 +0000

    Adding conditional text portlet

---
+++ plugins/portlets/conditional-text-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/conditionaltext/PreferenceKeys.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2010. M.I.T. All Rights Reserved
+ * Licensed under the MIT license. Please see http://www.opensource.org/licenses/mit-license.php
+ * or the license.txt file included in this distribution for the full text of the license.
+ */
+
+package org.climatecollaboratorium.conditionaltext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jintrone
+ * Date: Sep 11, 2010
+ * Time: 3:42:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PreferenceKeys {
+
+    public static String MSG_KEY="MSG_KEY";
+    public static String MSG_VAL="MSG_VAL";
+}
