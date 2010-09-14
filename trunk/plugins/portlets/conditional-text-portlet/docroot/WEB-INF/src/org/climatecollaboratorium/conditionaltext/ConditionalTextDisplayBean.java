87e8e740316445dc961340c215f69d78de3c40a0
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-09-14 22:00:26 +0000

    Little fix to cond text - not so small...

--- trunk/plugins/portlets/conditional-text-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/conditionaltext/ConditionalTextDisplayBean.java
+++ trunk/plugins/portlets/conditional-text-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/conditionaltext/ConditionalTextDisplayBean.java
@@ -123,6 +123,7 @@ public class ConditionalTextDisplayBean {
                 }
                 else {
                     value = null;
+                    refresh();
                 }
             }
             
