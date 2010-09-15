127a3c30906c06f6a097dcf1ef61c5bc846d1958
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-09-15 09:42:07 +0000

    Edit for newly created plans

--- trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/facelets/simulations/SimulationBean.java
+++ trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/facelets/simulations/SimulationBean.java
@@ -305,7 +305,7 @@ public class SimulationBean {
     }
     
 
-    public void init(Long simulationId, Long scenarioId) throws SystemException, IOException, IllegalUIConfigurationException {
+    public void init(Long simulationId, Long scenarioId, Boolean edit) throws SystemException, IOException, IllegalUIConfigurationException {
         if ((simulationId == lastInitSimulationId || (simulationId != null && simulationId.equals(lastInitSimulationId))) &&
                 (scenarioId == lastInitScenarioId || (scenarioId != null && scenarioId.equals(lastInitScenarioId))) ||
                 (lastInitSimulationId != null && lastInitSimulationId > 0 && simulationId == null || simulationId == 0)) {
@@ -314,6 +314,10 @@ public class SimulationBean {
         }
         lastInitSimulationId = simulationId;
         lastInitScenarioId = scenarioId;
+        
+        if (edit != null && edit) {
+            editActions(null);
+        }
         
         // check if scenario exist
         Exception exception = null; 
--- trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/facelets/simulations/support/SimulationSupportTag.java
+++ trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/facelets/simulations/support/SimulationSupportTag.java
@@ -20,6 +20,7 @@ public class SimulationSupportTag extends TagHandler{
     private final TagAttribute simulationBeanParam;
     private final TagAttribute simulationIdParam;
     private final TagAttribute scenarioIdParam;
+    private final TagAttribute editActionsParam;
     
     private static Log _log = LogFactoryUtil.getLog(SimulationSupportTag.class);
 
@@ -28,6 +29,7 @@ public class SimulationSupportTag extends TagHandler{
         simulationBeanParam = this.getRequiredAttribute("simulationBean");
         simulationIdParam = this.getAttribute("simulationId");
         scenarioIdParam = this.getAttribute("scenarioId");
+        editActionsParam = this.getAttribute("editActions");
     }
 
     @Override
@@ -38,8 +40,9 @@ public class SimulationSupportTag extends TagHandler{
         
         Long scenarioId = (Long) scenarioIdParam.getValueExpression(ctx, Long.class).getValue(ctx);
         Long simulationId = (Long) simulationIdParam.getValueExpression(ctx, Long.class).getValue(ctx);
+        Boolean edit = editActionsParam != null ? (Boolean) editActionsParam.getObject(ctx, Boolean.class) : false;
         try {
-            simulationBean.init(simulationId, scenarioId);
+            simulationBean.init(simulationId, scenarioId, edit);
         } catch (Exception e) {
             _log.error("Can't initialize simulationBean with parameters - simulationId " + 
                     simulationId + "\tscenarioId: " + scenarioId, e);
