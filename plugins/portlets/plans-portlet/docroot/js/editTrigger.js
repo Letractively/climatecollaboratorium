0881ae40502f0f1ceab4e4670f593ff2f40f616d
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-09-22 16:00:19 +0000

    Click to edit

---
+++ plugins/portlets/plans-portlet/docroot/js/editTrigger.js
@@ -0,0 +1,46 @@
+function initiailzeEditTrigger(isLoggedIn, canEdit, isPlanEditable) {
+	if (isPlanEditable) {
+		var message = "Log in to edit";
+		var callback = loginClickCallback;
+		if (isLoggedIn) {
+			if (canEdit) {
+				message = "Click to edit";
+				callback = editCallback;
+			}
+			else {
+				message = "Join the team to edit";
+				callback = joinTeamCallback;
+			}
+		}
+			
+		jQuery(".editTrigger .message").text(message);
+		clearEditTrigger();
+		jQuery(".editTriggerContainer").hover(function() {
+				jQuery(".editTrigger").show();
+			}, function() {
+				jQuery(".editTrigger").hide();
+			}
+		);
+		
+		jQuery(".editTrigger").click(function() { callback() });
+	}
+}
+
+function clearEditTrigger() {
+	jQuery(".editTriggerContainer").unbind('mouseenter mouseleave');
+	jQuery(".editTrigger").unbind("click");
+	jQuery(".editTrigger").hide();
+	
+}
+
+function loginClickCallback() {
+	showLoginPopup();
+}
+
+function joinTeamCallback() {
+	Collab.nav.navigateAddParams("plans", {tab: "team"}, false);
+}
+
+function editCallback() {
+	jQuery(".editStateTrigger").click();
+}
