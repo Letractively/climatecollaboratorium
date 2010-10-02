04207f149a93d779004f90ac2f49934290c5718b
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-10-02 23:09:02 +0000

    Always present "Click to edit" in assumptions and description

--- plugins/portlets/plans-portlet/docroot/js/editTrigger.js
+++ plugins/portlets/plans-portlet/docroot/js/editTrigger.js
@@ -4,49 +4,58 @@
  * or the license.txt file included in this distribution for the full text of the license.
  */
 
-function initiailzeEditTrigger(isLoggedIn, canEdit, isPlanEditable) {
-	if (isPlanEditable) {
-		var message = "Log in to edit";
-		var callback = loginClickCallback;
-		if (isLoggedIn) {
-			if (canEdit) {
-				message = "Click to edit";
-				callback = editCallback;
-			}
-			else {
-				message = "Join the team to edit";
-				callback = joinTeamCallback;
-			}
-		}
-			
-		jQuery(".editTrigger .message").text(message);
-		clearEditTrigger();
-		jQuery(".editTriggerContainer").hover(function() {
-				jQuery(".editTrigger").show();
-			}, function() {
-				jQuery(".editTrigger").hide();
-			}
-		);
-		
-		jQuery(".editTrigger").click(function() { callback() });
-	}
+function initiailzeEditTrigger(isLoggedIn, canEdit, isPlanEditable, alwaysshow) {
+    if (isPlanEditable) {
+        var message = "Log in to edit";
+        var callback = loginClickCallback;
+        if (isLoggedIn) {
+            if (canEdit) {
+                message = "Click to edit";
+                callback = editCallback;
+            }
+            else {
+                message = "Join the team to edit";
+                callback = joinTeamCallback;
+            }
+        }
+
+        jQuery(".editTrigger .message").text(message);
+        clearEditTrigger();
+        if (!alwaysshow) {
+         
+            jQuery(".editTriggerContainer").hover(function() {
+                jQuery(".editTrigger").show();
+            }, function() {
+                jQuery(".editTrigger").hide();
+            }
+                    );
+
+        } else {
+            jQuery(".editTrigger").show();
+        }
+
+
+        jQuery(".editTrigger").click(function() {
+            callback()
+        });
+    }
 }
 
 function clearEditTrigger() {
-	jQuery(".editTriggerContainer").unbind('mouseenter mouseleave');
-	jQuery(".editTrigger").unbind("click");
-	jQuery(".editTrigger").hide();
-	
+    jQuery(".editTriggerContainer").unbind('mouseenter mouseleave');
+    jQuery(".editTrigger").unbind("click");
+    jQuery(".editTrigger").hide();
+
 }
 
 function loginClickCallback() {
-	deferUntilLogin();
+    deferUntilLogin();
 }
 
 function joinTeamCallback() {
-	Collab.nav.navigateAddParams("plans", {tab: "team"}, false);
+    Collab.nav.navigateAddParams("plans", {tab: "team"}, false);
 }
 
 function editCallback() {
-	jQuery(".editStateTrigger").click();
+    jQuery(".editStateTrigger").click();
 }
