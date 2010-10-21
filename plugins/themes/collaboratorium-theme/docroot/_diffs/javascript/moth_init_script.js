d2cd6c132376c2cfb721fcf570615b573affb136
jintrone <jintrone@e9ce9132-09df-11df-a843-f99e4352f152>
2010-10-21 23:01:16 +0000

    Minor change

--- plugins/themes/collaboratorium-theme/docroot/_diffs/javascript/moth_init_script.js
+++ plugins/themes/collaboratorium-theme/docroot/_diffs/javascript/moth_init_script.js
@@ -64,7 +64,7 @@ function insertParam(key, value)
     key = escape(key); value = escape(value);
 
     var kvp = document.location.search.substr(1).split('&');
-
+    alert("kvp before is "+kvp);
     var i=kvp.length; var x; while(i--)
     {
         x = kvp[i].split('=');
@@ -78,7 +78,7 @@ function insertParam(key, value)
     }
 
     if(i<0) {kvp[kvp.length] = [key,value].join('=');}
-    alert("kvp="+kvp);
+    alert("kvp after is "+kvp);
     //this will reload the page, it's likely better to store this until finished
     document.location.search = kvp.length==1?kvp.join('&'):kvp;
 }
