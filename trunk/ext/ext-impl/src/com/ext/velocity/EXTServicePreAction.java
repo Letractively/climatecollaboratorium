5b3708871abeda2667716d235bcda8c9f8e7ef02
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-10-22 09:15:06 +0000

    Integration of new url scheme for plans

--- trunk/ext/ext-impl/src/com/ext/velocity/EXTServicePreAction.java
+++ trunk/ext/ext-impl/src/com/ext/velocity/EXTServicePreAction.java
@@ -13,8 +13,18 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ext.portlet.contests.NoSuchContestException;
+import com.ext.portlet.contests.model.Contest;
+import com.ext.portlet.contests.service.ContestLocalServiceUtil;
+import com.ext.portlet.plans.model.PlanItem;
+import com.ext.portlet.plans.service.PlanItemLocalService;
+import com.ext.portlet.plans.service.PlanItemLocalServiceUtil;
+import com.liferay.portal.PortalException;
+import com.liferay.portal.SystemException;
 import com.liferay.portal.events.ServicePreAction;
 import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.Theme;
 import com.liferay.portal.service.ThemeLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
@@ -23,24 +33,51 @@ import com.liferay.portal.util.WebKeys;
 public class EXTServicePreAction extends ServicePreAction {
     private static final String COLLABORATORIUM_THEME_NAME = "Collaboratorium";
     private static final String THEME_TIMESTAMP_ATTRIBUTE = "THEME_TIMESTAMP";
+    private static final Log _log = LogFactoryUtil.getLog(EXTServicePreAction.class);
 
-    public void run(HttpServletRequest req, HttpServletResponse res)
-    throws ActionException {
+    public void run(HttpServletRequest req, HttpServletResponse res) throws ActionException {
 
         Map<String, Object> vmVariables = new HashMap<String, Object>();
         ThemeDisplay themeDisplay = (ThemeDisplay) req.getAttribute(WebKeys.THEME_DISPLAY);
-        
-        
+
         List<Theme> themes = ThemeLocalServiceUtil.getThemes(themeDisplay.getCompanyId());
-        
+
         String themeTimestamp = "";
-        
-        for (Theme theme: themes) {
+
+        for (Theme theme : themes) {
             if (theme.getName().equals(COLLABORATORIUM_THEME_NAME)) {
                 themeTimestamp = String.valueOf(theme.getTimestamp());
             }
         }
-        
+
+        String contestIdStr = req.getParameter("_collab_paramcontestId");
+        if (contestIdStr != null) {
+            try {
+                Contest contest = ContestLocalServiceUtil.getContest(Long.parseLong(contestIdStr));
+                vmVariables.put("collab_contest", contest);
+            } catch (NumberFormatException e) {
+                _log.error("An exception has been thrown when trying to parse contest id " + contestIdStr, e);
+            } catch (PortalException e) {
+                _log.error("An exception has been thrown when loading contest with id " + contestIdStr, e);
+            } catch (SystemException e) {
+                _log.error("An exception has been thrown when loading contest with id " + contestIdStr, e);
+            }
+        }
+
+        String planIdStr = req.getParameter("_collab_paramplanId");
+        if (planIdStr != null) {
+            try {
+                PlanItem plan = PlanItemLocalServiceUtil.getPlan(Long.parseLong(planIdStr));
+                vmVariables.put("collab_plan", plan);
+            } catch (NumberFormatException e) {
+                _log.error("An exception has been thrown when trying to parse contest id " + contestIdStr, e);
+            } catch (PortalException e) {
+                _log.error("An exception has been thrown when loading contest with id " + contestIdStr, e);
+            } catch (SystemException e) {
+                _log.error("An exception has been thrown when loading contest with id " + contestIdStr, e);
+            }
+        }
+
         // Setup some variables
         vmVariables.put("themeTimestamp", themeTimestamp);
         req.setAttribute(WebKeys.VM_VARIABLES, vmVariables);
