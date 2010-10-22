eea7c66c443143480d02ebb757f5e5f812ef0d3e
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-10-22 09:10:02 +0000

    Integration of new url scheme for plans

--- trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/com/climatecollaboratorium/navigation/SharedFriendlyURLMapper.java
+++ trunk/plugins/portlets/sharedcollabfacelets-portlet/docroot/WEB-INF/src/com/climatecollaboratorium/navigation/SharedFriendlyURLMapper.java
@@ -8,7 +8,7 @@ import com.liferay.portal.kernel.portlet.LiferayPortletURL;
 public class SharedFriendlyURLMapper extends BaseFriendlyURLMapper{
     private static final String _MAPPING = "shared";
 
-    private static final String _PORTLET_ID = "sharedcollabfacelets";
+    private static final String _PORTLET_ID = "sharedcollabfacelets_WAR_sharedcollabfaceletsportlet";
 
     
     @Override
@@ -19,6 +19,7 @@ public class SharedFriendlyURLMapper extends BaseFriendlyURLMapper{
     @Override
     public String buildPath(LiferayPortletURL portletURL) {
         // TODO Auto-generated method stub
+        ///System.out.println("Liferay portlet url: " + portletURL);
         return "/shared/";
     }
 
@@ -29,6 +30,18 @@ public class SharedFriendlyURLMapper extends BaseFriendlyURLMapper{
 
     @Override
     public void populateParams(String friendlyURLPath, Map<String, String[]> params) {
+        System.out.println("friendlyURLPath: " + friendlyURLPath);
+        System.out.println("params: " + params);
+        
+        addParam(params, "p_p_id", _PORTLET_ID);
+        //addParam(params, "p_p_lifecycle", "2");
+        addParam(params, "planId", "3333");
+
+        params.put("planId", new String[] {"3306"});
+        
+        System.out.println(params);
+        
+        
     }
     
     
