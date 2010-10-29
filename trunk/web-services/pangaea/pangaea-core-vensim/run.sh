627eaed85825113002e5e99f82a5f294989f6116
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-10-29 16:59:09 +0000

    Updating C-Learn to correct vals

--- trunk/web-services/pangaea/pangaea-core-vensim/run.sh
+++ trunk/web-services/pangaea/pangaea-core-vensim/run.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH="/home/collab/pangaea_vensim/vensim_dll"
 export JAVA_OPTS="-Dvensim_lib_name=vendll32 -Dvensim_model_path=/home/collab/pangaea_vensim/vensim_model/clearn.vmf"
 
 
-${JAVA_HOME}/bin/java ${JAVA_OPTS} -cp lib/log4j-1.2.15.jar:lib/commons-logging-1.1.1.jar:lib/Vensim.jar:build/classes org.climatecollaboratorium.pangaea.vensim.PangaeaConnection
+${JAVA_HOME}/bin/java ${JAVA_OPTS} -cp lib/log4j-1.2.15.jar:lib/commons-logging-1.1.1.jar:lib/Vensim.jar:build/pangaea-core.jar org.climatecollaboratorium.pangaea.vensim.PangaeaConnection
