955e801ce8ba427d7544b5fd507cb8392e5e41f4
janusz.parfieniuk <janusz.parfieniuk@e9ce9132-09df-11df-a843-f99e4352f152>
2010-12-05 22:29:00 +0000

    labels for start and target year

---
+++ trunk/ext/sql/collaboratorium/typhon_0.1.2/start_target_year_descriptions.sql
@@ -0,0 +1,8 @@
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for developed countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =612;
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for developed countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =606;
+
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for rapidly developing countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =608;
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for rapidly developing countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =614;
+
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for other developing countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =607;
+UPDATE  `lportal`.`ModelInputGroup` SET  `description` =  'Start year is the year when reduction begins and target year is the year at which reduction will be complete for other developing countries.' WHERE  `ModelInputGroup`.`modelInputGroupPK` =613;
