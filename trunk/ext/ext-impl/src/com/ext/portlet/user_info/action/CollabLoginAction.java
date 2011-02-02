2fa28c5e0e940403611bc88298b406facc325fe1
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-02-02 19:17:03 +0000

    login/reg popups

---
+++ trunk/ext/ext-impl/src/com/ext/portlet/user_info/action/CollabLoginAction.java
@@ -0,0 +1,47 @@
+package com.ext.portlet.user_info.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portlet.login.action.LoginAction;
+
+public class CollabLoginAction extends LoginAction {
+
+    public void processAction(
+            ActionMapping mapping, ActionForm form, PortletConfig portletConfig,
+            ActionRequest actionRequest, ActionResponse actionResponse)
+        throws Exception {
+        
+        String redirect = ParamUtil.getString(actionRequest, "redirect");
+        
+        
+        redirect = Helper.removeParamFromRequestStr(redirect, "signinRegError");
+        redirect = Helper.removeParamFromRequestStr(redirect, "isSigningInPopup");
+        redirect = Helper.removeParamFromRequestStr(redirect, "isSigningIn");
+        
+        super.processAction(mapping, form, portletConfig, actionRequest, actionResponse);
+        
+        if (! SessionErrors.isEmpty(actionRequest)) {
+            // url parameters
+            Map<String, String> parameters = new HashMap<String, String>();
+            //boolean isSigningInPopup = ParamUtil.getBoolean(actionRequest, "isSigningInPopup");
+
+            parameters.put("isSigningIn", "true");
+
+            redirect = Helper.modifyRedirectUrl(redirect, actionRequest, parameters);
+            
+        }
+        
+        actionResponse.sendRedirect(redirect);
+    }
+
+}
