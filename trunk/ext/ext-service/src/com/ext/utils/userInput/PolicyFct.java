6288ef34cc36fec1b9f74eff08c13e0c8f04512b
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-04-16 23:19:49 +0000

    Problems with factory bean

--- trunk/ext/ext-service/src/com/ext/utils/userInput/PolicyFct.java
+++ trunk/ext/ext-service/src/com/ext/utils/userInput/PolicyFct.java
@@ -1,33 +1,18 @@
 package com.ext.utils.userInput;
 
 import org.owasp.validator.html.Policy;
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 
-public class PolicyFactoryBean implements FactoryBean, InitializingBean {
+public class PolicyFct {
     Policy policy;
 
     Resource policyFile;
-
-    @Override
-    public Policy getObject() throws Exception {
-        // TODO Auto-generated method stub
+    
+    
+    public Policy getPolicy() {
         return policy;
     }
 
-    @Override
-    public Class<?> getObjectType() {
-        // TODO Auto-generated method stub
-        return Policy.class;
-    }
-
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
-
-    @Override
     public void afterPropertiesSet() throws Exception {
         policy = Policy.getInstance(policyFile.getURL());
     }
