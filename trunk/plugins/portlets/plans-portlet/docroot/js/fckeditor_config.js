c8c3aa1e90af5c768f2380652412da40ab71f646
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-06-19 20:37:12 +0000

    better editor

---
+++ trunk/plugins/portlets/plans-portlet/docroot/js/fckeditor_config.js
@@ -0,0 +1,8 @@
+FCKConfig.CleanWordKeepsStructure = true ;
+FCKConfig.ToolbarSets['colabTab'] = [['Bold','Italic','Underline', 'Style' , '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 'Table', 'Image', 'Link', 'SpecialChar', 'Source']];
+FCKConfig.CleanWordKeepsStructure = true ;
+FCKConfig.ForcePasteAsPlainText = true;
+FCKConfig.CustomStyles = { };
+FCKConfig.StylesXmlPath = '../../../../js/fckstyles.xml' ;
+FCKConfig.EditorAreaCSS = '../../../../../collaboratorium-theme/css/fckeditor_styles.css' ;
+FCKConfig.BodyId = 'main';
