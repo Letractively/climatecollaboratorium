a0ee0c0b913c322ced261800fd98da9038d5a49e
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-06-22 20:29:34 +0000

    Fixes in debates permissions

--- trunk/plugins/portlets/debatebrowser-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/debatebrowser/DebateBrowserPermissions.java
+++ trunk/plugins/portlets/debatebrowser-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/debatebrowser/DebateBrowserPermissions.java
@@ -4,16 +4,25 @@ import org.climatecollaboratorium.debatebrowser.utils.Helper;
 
 public class DebateBrowserPermissions {
     
+    private final String CAN_ADMIN_ACTION = "DEBATES_CAN_ADMIN";
+    
+    public DebateBrowserPermissions() {
+        
+    }
+    
     public boolean getCanAddCategory() {
-        return Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
+                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
     }
     
     public boolean getCanEditCategory() {
-        return Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
+                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
     }
 
     public boolean getCanAddDebate() {
-        return Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
+                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
     }
     
 }
