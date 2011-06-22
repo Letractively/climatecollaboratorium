91204b7fc59b0ac43fb8c0fa4843ce6f3dcb01fc
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-06-22 20:29:18 +0000

    Fixes in debates permissions

--- trunk/plugins/portlets/plans-portlet/docroot/js/fckeditor_config.js
+++ trunk/plugins/portlets/plans-portlet/docroot/js/fckeditor_config.js
@@ -1,7 +1,9 @@
 FCKConfig.CleanWordKeepsStructure = true ;
-FCKConfig.ToolbarSets['colabTab'] = [['Bold','Italic','Underline', 'Style' , '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 'Table', 'Image', 'Link', 'SpecialChar', 'Source']];
+FCKConfig.ToolbarSets['colabTab'] = [['Bold','Italic','Underline', 'Style' , '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 'Table', 'Image', 'Link', 'SpecialChar', 'Source', 'PasteWord']];
 FCKConfig.CleanWordKeepsStructure = true ;
-FCKConfig.ForcePasteAsPlainText = true;
+//FCKConfig.ForcePasteAsPlainText = true;
+FCKConfig.CleanWordKeepsStructure = true ;
+FCKConfig.AutoDetectPasteFromWord = true;
 FCKConfig.CustomStyles = { };
 FCKConfig.StylesXmlPath = '../../../../js/fckstyles.xml' ;
 FCKConfig.EditorAreaCSS = '../../../../../collaboratorium-theme/css/fckeditor_styles.css' ;
