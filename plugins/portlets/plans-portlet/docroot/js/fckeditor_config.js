6566fa19c6440a8c8ca0eca4ebdf7ae05309bf1e
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2011-06-27 22:52:35 +0000

    paste from word enabled

--- plugins/portlets/plans-portlet/docroot/js/fckeditor_config.js
+++ plugins/portlets/plans-portlet/docroot/js/fckeditor_config.js
@@ -1,9 +1,7 @@
 FCKConfig.CleanWordKeepsStructure = true ;
 FCKConfig.ToolbarSets['colabTab'] = [['Bold','Italic','Underline', 'Style' , '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 'Table', 'Image', 'Link', 'SpecialChar', 'Source', 'PasteWord']];
+//FCKConfig.ForcePasteAsPlainText = true;
 FCKConfig.CleanWordKeepsStructure = true ;
-FCKConfig.ForcePasteAsPlainText = true;
-FCKConfig.CleanWordKeepsStructure = true ;
-FCKConfig.AutoDetectPasteFromWord = true;
 FCKConfig.CustomStyles = { };
 FCKConfig.StylesXmlPath = '../../../../js/fckstyles.xml' ;
 FCKConfig.EditorAreaCSS = '../../../../../collaboratorium-theme/css/fckeditor_styles.css' ;
