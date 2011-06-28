4d37e2433aabdf5ddef0e0045e8c5341a739123c
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2011-06-28 07:26:54 +0000

    debate browser permissions fix

--- plugins/portlets/debatebrowser-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/debatebrowser/DebateBrowserPermissions.java
+++ plugins/portlets/debatebrowser-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/debatebrowser/DebateBrowserPermissions.java
@@ -5,24 +5,29 @@ import org.climatecollaboratorium.debatebrowser.utils.Helper;
 public class DebateBrowserPermissions {
     
     private final String CAN_ADMIN_ACTION = "DEBATES_CAN_ADMIN";
+    private final String primKey;
+    private final String rootPortletId;
+    private final Long groupId;
     
     public DebateBrowserPermissions() {
-        
+        groupId = Helper.getGroupId();
+        rootPortletId = Helper.getRootPortletId();
+        primKey = Helper.getPrimKey();
     }
     
     public boolean getCanAddCategory() {
-        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
-                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(groupId, rootPortletId, primKey, CAN_ADMIN_ACTION) || 
+        Helper.getPermissionChecker().isOmniadmin();
     }
     
     public boolean getCanEditCategory() {
-        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
-                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(groupId, rootPortletId, 
+                primKey, CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
     }
 
     public boolean getCanAddDebate() {
-        return Helper.getPermissionChecker().hasPermission(Helper.getGroupId(), Helper.getRootPortletId(), 
-                Helper.getPrimKey(), CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
+        return Helper.getPermissionChecker().hasPermission(groupId, rootPortletId, 
+                primKey, CAN_ADMIN_ACTION) || Helper.getPermissionChecker().isOmniadmin();
     }
     
 }
