1ea1461aa0588e1ffaf24ab6458179a5d2933606
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2011-06-30 06:42:17 +0000

    Customized email messages on activity support

--- ext/sql/collaboratorium/typhon_0.1.4/messagingUserPreferences.sql
+++ ext/sql/collaboratorium/typhon_0.1.4/messagingUserPreferences.sql
@@ -1,2 +1,2 @@
-ALTER TABLE  `MessagingUserPreferences` ADD  `emailOnActivity` TINYINT( 1 ) NOT NULL DEFAULT 1;
+ALTER TABLE  `MessagingUserPreferences` ADD  `emailOnActivity` TINYINT( 1 ) DEFAULT 1;
 UPDATE MessagingUserPreferences SET emailOnActivity = 1;
