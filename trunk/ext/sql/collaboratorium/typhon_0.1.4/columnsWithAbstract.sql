76fdf001a85a1d22a7440438a1a54ff048ee7b94
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2011-07-07 16:08:50 +0000

    Pitch in plans constants

--- trunk/ext/sql/collaboratorium/typhon_0.1.4/columnsWithAbstract.sql
+++ trunk/ext/sql/collaboratorium/typhon_0.1.4/columnsWithAbstract.sql
@@ -1,4 +1,7 @@
 delete from ContestPhaseColumn WHERE ContestPhasePK = 8;
-insert into ContestPhaseColumn Values(81, 8, 'ABSTRACT', 10, 0);
-insert into ContestPhaseColumn Values(82, 8, 'SEEKING_ASSISTANCE', 20, 0);
-insert into ContestPhaseColumn Values(83, 8, 'CREATOR', 30, 0);
+insert into ContestPhaseColumn Values(83, 8, 'CREATOR', 10, 0);
+insert into ContestPhaseColumn Values(81, 8, 'ABSTRACT', 20, 0);
+insert into ContestPhaseColumn Values(82, 8, 'SUPPORTERS', 30, 0);
+
+
+insert into PlanTypeAttribute values (42,3,'ABSTRACT');
