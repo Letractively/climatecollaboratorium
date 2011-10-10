62c55966472176400898a2710ba6dc65265dd211
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2011-10-10 16:27:38 +0000

    Sql modifications to new phases

---
+++ ext/sql/collaboratorium/typhon_0.1.6/movePlanToNational.sql
@@ -0,0 +1,6 @@
+Update PlanMeta set ContestPhase = 9, planTypeId = 4 where planId = 15056;
+
+Update DiscussionCategoryGroup set url = '/web/guest/plans/-/plans/contestId/5/planId/15056#plans=tab:comments' where id_ = 15056;
+
+delete from PlanAttribute where planId = 15056 and attributeName = 'REGION';
+insert into PlanAttribute VALUES (134672, 15056, 'REGION', 'South Africa');
