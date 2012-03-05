9971f21ce0be72421047479be658f35dd38efc31
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2012-03-05 19:25:40 +0000

    Admin screens for referencing focus area from plansectiondefinition/ contest

--- plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/wrappers/ContestWrapper.java
+++ plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/wrappers/ContestWrapper.java
@@ -3,11 +3,20 @@ package org.climatecollaboratorium.plans.admin.wrappers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 
 import com.ext.portlet.contests.model.Contest;
+import com.ext.portlet.contests.model.ContestPhase;
+import com.ext.portlet.ontology.model.FocusArea;
+import com.ext.portlet.ontology.model.OntologyTermEntity;
+import com.ext.portlet.ontology.service.FocusAreaLocalServiceUtil;
+import com.ext.portlet.ontology.service.OntologyTermEntityLocalServiceUtil;
+import com.ext.portlet.ontology.service.OntologyTermLocalServiceUtil;
+import com.ext.portlet.plans.model.PlanItem;
 import com.ext.portlet.plans.model.PlanTemplate;
+import com.ext.portlet.plans.service.PlanItemLocalServiceUtil;
 import com.ext.portlet.plans.service.PlanTemplateLocalServiceUtil;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
@@ -49,6 +58,15 @@ public class ContestWrapper {
         return ret;
     }
     
+    public List<SelectItem> getAvailableFocusAreas() throws SystemException {
+        List<SelectItem> ret = new ArrayList<SelectItem>();
+        
+        for (FocusArea fa: FocusAreaLocalServiceUtil.getFocusAreas(0, Integer.MAX_VALUE)) {
+            ret.add(new SelectItem(fa.getId(), fa.getName()));
+        }
+        return ret;
+    }
+    
 
     public void planTemplateChange(ValueChangeEvent e) throws NumberFormatException, PortalException, SystemException {
         PlanTemplate tmpl = PlanTemplateLocalServiceUtil.getPlanTemplate(Long.parseLong(e.getNewValue().toString()));
@@ -56,6 +74,24 @@ public class ContestWrapper {
         contest.setPlanTemplateId(tmpl.getId());
         contest.store();
     }
+    
+    public void planFocusAreaChange(ValueChangeEvent e) throws NumberFormatException, PortalException, SystemException {
+        FocusArea fa = FocusAreaLocalServiceUtil.getFocusArea(Long.parseLong(e.getNewValue().toString()));
+        
+        contest.setFocusAreaId(fa.getId());
+        contest.store();
+    }
+    
+    public void populateFAtoPlans(ActionEvent e) throws PortalException, SystemException {
+        FocusArea fa = contest.getFocusArea();
+        
+        for (ContestPhase phase: contest.getPhases()) {
+            for (PlanItem plan: PlanItemLocalServiceUtil.getPlansInContestPhase(phase)) {
+                fa.tagClass(PlanItem.class, plan.getPlanId());
+            }
+        }
+    }
+    
     
 
 }
