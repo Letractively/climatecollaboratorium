9e5df182065ab62e7d4b65b3f7892604cb8332b5
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2012-03-05 19:25:40 +0000

    Admin screens for referencing focus area from plansectiondefinition/ contest

--- branches/typchon_0.2.0/plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/wrappers/PlanSectionDefinitionWrapper.java
+++ branches/typchon_0.2.0/plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/wrappers/PlanSectionDefinitionWrapper.java
@@ -1,6 +1,15 @@
 package org.climatecollaboratorium.plans.admin.wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+
+import com.ext.portlet.ontology.model.FocusArea;
+import com.ext.portlet.ontology.service.FocusAreaLocalServiceUtil;
 import com.ext.portlet.plans.model.PlanSectionDefinition;
+import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 
 public class PlanSectionDefinitionWrapper {
@@ -26,6 +35,22 @@ public class PlanSectionDefinitionWrapper {
         definition.store();
         
         return "backToIndex";
+    }
+    
+    public void focusAreaChange(ValueChangeEvent e) throws NumberFormatException, PortalException, SystemException {
+        FocusArea fa = FocusAreaLocalServiceUtil.getFocusArea(Long.parseLong(e.getNewValue().toString()));
+        
+        definition.setFocusAreaId(fa.getId());
+        definition.store();
+    }
+    
+    public List<SelectItem> getAvailableFocusAreas() throws SystemException {
+        List<SelectItem> ret = new ArrayList<SelectItem>();
+        
+        for (FocusArea fa: FocusAreaLocalServiceUtil.getFocusAreas(0, Integer.MAX_VALUE)) {
+            ret.add(new SelectItem(fa.getId(), fa.getName()));
+        }
+        return ret;
     }
     
     
