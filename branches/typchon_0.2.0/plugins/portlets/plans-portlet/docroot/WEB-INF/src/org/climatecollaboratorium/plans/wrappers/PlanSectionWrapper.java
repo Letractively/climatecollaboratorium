1a2217d52d2bce8a7abec39f7caf1c753a381557
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2012-03-05 19:26:01 +0000

    Display of sections with simple edit widget

--- branches/typchon_0.2.0/plugins/portlets/plans-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/wrappers/PlanSectionWrapper.java
+++ branches/typchon_0.2.0/plugins/portlets/plans-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/wrappers/PlanSectionWrapper.java
@@ -1,10 +1,15 @@
 package org.climatecollaboratorium.plans.wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 
 import org.climatecollaboratorium.plans.Helper;
 
 import com.ext.portlet.plans.NoSuchPlanItemException;
+import com.ext.portlet.plans.model.PlanItem;
 import com.ext.portlet.plans.model.PlanSection;
 import com.ext.portlet.plans.service.PlanItemLocalServiceUtil;
 import com.ext.portlet.plans.service.PlanLocalServiceUtil;
@@ -15,10 +20,15 @@ public class PlanSectionWrapper {
     private PlanSection section;
     private boolean editing;
     private PlanItemWrapper piw;
+    private List<Long> referencedPlans;
 
-    public PlanSectionWrapper(PlanSection section, PlanItemWrapper planItemWrapper) {
+    public PlanSectionWrapper(PlanSection section, PlanItemWrapper planItemWrapper) throws NoSuchPlanItemException, SystemException {
         this.section = section;
         piw = planItemWrapper;
+        referencedPlans = new ArrayList<Long>();
+        for (PlanItem plan: section.getReferencedPlans()) {
+            referencedPlans.add(plan.getId());
+        }
         
         
     }
@@ -43,11 +53,40 @@ public class PlanSectionWrapper {
         if (Helper.isUserLoggedIn()) {
             System.out.println("new content: " + section.getContent());
             PlanItemLocalServiceUtil.getPlan(section.getPlanId())
-                    .setSectionContent(section.getDefinition(), section.getContent(), Helper.getLiferayUser().getUserId());
+                    .setSectionContent(section.getDefinition(), section.getContent(), referencedPlans, Helper.getLiferayUser().getUserId());
             piw.refresh();
+            
+            
         }
         toggleEditing(e);
     }
+    
+    public List<SelectItem> getPlansForReference() throws PortalException, SystemException {
+        List<SelectItem> ret = new ArrayList<SelectItem>();
+        
+        if (section.getDefinition().getFocusArea() != null) {
+            for (PlanItem plan: PlanItemLocalServiceUtil.findPlansForFocusArea(section.getDefinition().getFocusArea())) {
+                ret.add(new SelectItem(plan.getPlanId(), plan.getName()));
+            }
+        }
+
+        return ret;
+    }
+    
+    public List<Long> getReferencedPlanIds() throws NoSuchPlanItemException, SystemException {
+        return referencedPlans;
+    }
+    
+    public void setReferencedPlanIds(List<String> plans) {
+        referencedPlans.clear();
+        for (String planId: plans) {
+            referencedPlans.add(Long.parseLong(planId));
+        }
+        
+        
+    }
+    
+    
     
 
 }
