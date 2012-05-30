177f176980892b4ce14bfbac0c079d1d32ddb8e5
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2012-05-30 21:03:33 +0000

    discussion theming

--- branches/typchon_0.2.0/plugins/themes/climatecolab-theme/docroot/_diffs/javascript/moth_init_script.js
+++ branches/typchon_0.2.0/plugins/themes/climatecolab-theme/docroot/_diffs/javascript/moth_init_script.js
@@ -385,3 +385,18 @@ function updateBreadcrumb(placeholder, items) {
 			}
 		} 
 	}
+
+
+	function submitenter(myfield,e)	{
+		var keycode;
+		if (window.event) keycode = window.event.keyCode;
+		else if (e) keycode = e.which;
+		else return true;
+
+		if (keycode == 13) {
+			jQuery(myfield.form).submit();
+			return false;
+		}
+		else
+			return true;
+	}
