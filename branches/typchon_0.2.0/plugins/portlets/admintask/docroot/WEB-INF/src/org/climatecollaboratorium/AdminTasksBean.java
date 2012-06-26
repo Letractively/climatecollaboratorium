1f07abc1773f88b3371868e893ea10c0e86bdb11
janusz.parfieniuk@gmail.com <janusz.parfieniuk@gmail.com@e9ce9132-09df-11df-a843-f99e4352f152>
2012-06-26 07:13:03 +0000

    Separate portlet for admin tasks

---
+++ branches/typchon_0.2.0/plugins/portlets/admintask/docroot/WEB-INF/src/org/climatecollaboratorium/AdminTasksBean.java
@@ -0,0 +1,61 @@
+package org.climatecollaboratorium;
+
+import java.util.List;
+
+import org.climatecollaboratorium.facelets.plans.wrappers.PlanItemWrapper;
+
+import com.ext.portlet.plans.PlanConstants;
+import com.ext.portlet.plans.model.PlanAttribute;
+import com.ext.portlet.plans.model.PlanDescription;
+import com.ext.portlet.plans.model.PlanItem;
+import com.ext.portlet.plans.service.PlanItemLocalServiceUtil;
+import com.liferay.portal.SystemException;
+import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
+
+public class AdminTasksBean {
+
+    public String removeBlogsFormatting() throws SystemException {
+        for (BlogsEntry entry: BlogsEntryLocalServiceUtil.getBlogsEntries(0, Integer.MAX_VALUE)) {
+            
+            String content = entry.getContent();
+            
+            content = ContentFilteringHelper.removeStylingFromHTMLContent(content);
+            
+            entry.setContent(content);
+            BlogsEntryLocalServiceUtil.updateBlogsEntry(entry);
+        }
+        return null;
+    }
+    
+    public String removeProposalFormatting() throws SystemException {
+        for (PlanItem plan: PlanItemLocalServiceUtil.getPlans()) {
+
+            String description = plan.getDescription();
+            if (description != null) {
+                description = ContentFilteringHelper.removeStylingFromHTMLContent(description);
+            }
+            List<PlanDescription> descriptions = plan.getPlanDescriptions();
+            PlanDescription planDescriptionLast = descriptions.get(descriptions.size()-1);
+            planDescriptionLast.setDescription(description);
+            planDescriptionLast.store();
+        }
+        return null;
+    }
+    
+    public String updatePitchForProposals() throws SystemException {
+        for (PlanItem plan: PlanItemLocalServiceUtil.getPlans()) {
+            String pitch = plan.getPitch();
+            if (pitch == null || pitch.trim().length() == 0) {
+                PlanAttribute attr = plan.getPlanAttribute(PlanConstants.Attribute.ABSTRACT.name());
+                if (attr != null && attr.getAttributeValue() != null) {
+                    List<PlanDescription> descriptions = plan.getPlanDescriptions();
+                    PlanDescription planDescriptionLast = descriptions.get(descriptions.size()-1);
+                    planDescriptionLast.setPitch(attr.getAttributeValue());
+                    planDescriptionLast.store();
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ branches/typchon_0.2.0/plugins/portlets/admintask/docroot/WEB-INF/src/org/climatecollaboratorium/ContentFilteringHelper.java
@@ -0,0 +1,55 @@
+package org.climatecollaboratorium;
+
+public class ContentFilteringHelper {
+    
+    public static String removeStylingFromHTMLContent(String content) {
+
+        content = removeTagWithContents(content, "xml");
+        content = removeTagWithContents(content, "style");
+        content = removeAttribute(content, "style");
+        content = removeAttribute(content, "border");
+        content = removeTagLeaveContents(content, "font");
+        
+        return content;
+    }
+
+    private static String removeTagWithContents(String content, String tagName) {
+        int startPos = content.indexOf("<" + tagName);
+        while (startPos >= 0) {
+            int endPos = content.indexOf("</" + tagName, startPos);
+            endPos = content.indexOf(">", endPos) + 1;
+            
+            content = content.substring(0, startPos) + content.substring(endPos);
+            startPos = content.indexOf("<" + tagName);
+        }
+            
+        
+        return content;
+    }
+    
+    private static String removeTagLeaveContents(String content, String tagName) {
+        int startPos = content.indexOf("<" + tagName);
+        while (startPos >= 0) {
+            int endPos = content.indexOf(">", startPos) + 1;
+            
+            content = content.substring(0, startPos) + content.substring(endPos);
+            startPos = content.indexOf("<" + tagName);
+        }
+        startPos = content.indexOf("</" + tagName);
+        while (startPos >= 0) {
+            int endPos = content.indexOf(">", startPos) + 1;
+            
+            content = content.substring(0, startPos) + content.substring(endPos);
+            startPos = content.indexOf("</" + tagName);
+        }
+            
+        
+        return content;
+    }
+    
+    private static String removeAttribute(String content, String name) {
+        return content.replaceAll(name + "=\"[^\"]*\"", "");
+        
+    }
+    
+}
