587909a4c45ae7f7d9fc5cad6b92358fcbda2c1e
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2012-07-01 11:32:51 +0000

    - Admin tool to strip formatting from old plans handles "h" tags incorrectly

--- plugins/portlets/admintask-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/ContentFilteringHelper.java
+++ plugins/portlets/admintask-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/ContentFilteringHelper.java
@@ -1,6 +1,24 @@
 package org.climatecollaboratorium;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ContentFilteringHelper {
+    
+    private static Map<String, String[]> replacementTags = new HashMap<String, String[]>();
+    
+    static {
+        replacementTags.put("h1", new String[] {"p", "strong"});
+        replacementTags.put("h2", new String[] {"p", "strong"});
+        replacementTags.put("h3", new String[] {"p", "strong"});
+        replacementTags.put("h4", new String[] {"p", "strong"});
+        replacementTags.put("h5", new String[] {"p", "strong"});
+        replacementTags.put("h6", new String[] {"p", "strong"});
+        replacementTags.put("sub", new String[] {"span"});
+        replacementTags.put("sup", new String[] {"span"});
+    }
     
     public static String removeStylingFromHTMLContent(String content) {
 
@@ -9,6 +27,10 @@ public class ContentFilteringHelper {
         content = removeAttribute(content, "style");
         content = removeAttribute(content, "border");
         content = removeTagLeaveContents(content, "font");
+        
+        for (Map.Entry<String, String[]> entry: replacementTags.entrySet()) {
+            content = replaceTagWithTag(content, entry.getKey(), entry.getValue());
+        }
         
         return content;
     }
@@ -47,9 +69,40 @@ public class ContentFilteringHelper {
         return content;
     }
     
+    private static String replaceTagWithTag(String content, String fromTag, String...toTags) {
+        StringBuilder sb = new StringBuilder();
+        Pattern p = Pattern.compile("<(/?)" + fromTag + "[^>]*>");
+        Matcher m = p.matcher(content);
+        int prev = 0;
+        while (m.find()) {
+            // copy everything from previous tag end till < character
+            sb.append(content, prev, m.start());
+            // if there is 1 group then it means it's closing tag
+            for (String toTag: toTags) {
+                sb.append("<");
+                if (m.group(1).length() > 0) {
+                    sb.append("/");
+                }
+                sb.append(toTag);
+                sb.append(">");
+            }
+            prev = m.end();
+        }
+        sb.append(content, prev, content.length());
+        
+        return sb.toString();
+    }
+    
     private static String removeAttribute(String content, String name) {
         return content.replaceAll(name + "=\"[^\"]*\"", "");
         
+    }
+    
+    public static void main(String[] args) {
+        String tmp = replaceTagWithTag("<h1>fasdfasdfasdf</h1> asdfasdfasdfasdfa <span>..........</span>fasdfasdfasdf", "h1", "p");
+        System.out.println(tmp);
+        tmp = replaceTagWithTag(tmp, "span", "p", "strong");
+        System.out.println(tmp);
     }
     
 }
