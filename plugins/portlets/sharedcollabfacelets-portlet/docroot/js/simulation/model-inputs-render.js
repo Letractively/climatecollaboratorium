980c09128db79db82db7802394c3e7ea525b2d8f
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2012-07-02 13:13:51 +0000

    legend location fix

--- plugins/portlets/sharedcollabfacelets-portlet/docroot/js/simulation/model-inputs-render.js
+++ plugins/portlets/sharedcollabfacelets-portlet/docroot/js/simulation/model-inputs-render.js
@@ -640,36 +640,6 @@ function renderSingleChart(chartDef) {
 
 				
 				var plot = jQuery.jqplot(chartPlaceholderId, values, plotOptions); 
-				/*var plot3 = jQuery.jqplot(chartPlaceholderId, [cosPoints, sinPoints, powPoints1, powPoints2], 
-					    { 
-					      title:'Line Style Options', 
-					      // Series options are specified as an array of objects, one object
-					      // for each series.
-					      series:[ 
-					          {
-					            // Change our line width and use a diamond shaped marker.
-					            lineWidth:2, 
-					            markerOptions: { style:'dimaond' }
-					          }, 
-					          {
-					            // Don't show a line, just show markers.
-					            // Make the markers 7 pixels with an 'x' style
-					            showLine:false, 
-					            markerOptions: { size: 7, style:"x" }
-					          },
-					          { 
-					            // Use (open) circlular markers.
-					            markerOptions: { style:"circle" }
-					          }, 
-					          {
-					            // Use a thicker, 5 pixel line and 10 pixel
-					            // filled square markers.
-					            lineWidth:5, 
-					            markerOptions: { style:"filledSquare", size:10 }
-					          }
-					      ]
-					    }
-					  );*/
 
 			} catch (e) {
 
