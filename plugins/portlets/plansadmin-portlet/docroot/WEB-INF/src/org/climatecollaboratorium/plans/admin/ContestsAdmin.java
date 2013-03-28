80ae6f77032b7b9091962f6a60235abd6192b139
janusz.parfieniuk <janusz.parfieniuk@gmail.com>
2013-03-28 20:37:54 +0000

    Creation of a contest...

--- plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/ContestsAdmin.java
+++ plugins/portlets/plansadmin-portlet/docroot/WEB-INF/src/org/climatecollaboratorium/plans/admin/ContestsAdmin.java
@@ -1,6 +1,7 @@
 package org.climatecollaboratorium.plans.admin;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -9,8 +10,11 @@ import javax.faces.event.ActionEvent;
 import org.climatecollaboratorium.plans.admin.wrappers.ContestWrapper;
 
 import com.ext.portlet.contests.NoSuchContestException;
+import com.ext.portlet.contests.NoSuchContestPhaseException;
 import com.ext.portlet.contests.model.Contest;
+import com.ext.portlet.contests.model.ContestPhase;
 import com.ext.portlet.contests.service.ContestLocalServiceUtil;
+import com.ext.portlet.contests.service.ContestPhaseLocalServiceUtil;
 import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
@@ -53,6 +57,22 @@ public class ContestsAdmin {
     		}
     		
     	}
+    	
+    	while (true) {
+    		try {
+    			long contestPhasePk = CounterLocalServiceUtil.increment(ContestPhase.class.getName());
+    		
+    			ContestPhaseLocalServiceUtil.getContestPhase(contestPhasePk);
+    		}
+    		catch (NoSuchContestPhaseException ex) {
+    			break;
+    		}
+    		catch (Exception ex) {
+    			ex.printStackTrace();
+    		}
+    		
+    	}
+    	
 
 		User user = UserLocalServiceUtil.getUser(10144L);
 		long userId = user.getUserId();
@@ -73,6 +93,29 @@ public class ContestsAdmin {
 		PermissionThreadLocal.setPermissionChecker(permissionChecker);
 		
 		Contest contest = ContestLocalServiceUtil.createNewContest(10144L, "New content name bla bla bla " + new Random().nextLong());
+
+    	contest.setContestActive(true);
+    	contest.setWeight(0);
+    	contest.setCreated(new Date());
+    	contest.setFeatured(false);
+    	contest.setFlag(0);
+    	contest.setPlanTypeId(6L);
+    	contest.setUpdated(new Date());
+    	contest.store();
+    	
+    	ContestPhase phase = ContestPhaseLocalServiceUtil.createContestPhase(CounterLocalServiceUtil.increment(ContestPhase.class.getName()));
+    	
+    	phase.setPhaseStartDate(new Date());
+    	phase.setPhaseEndDate(new Date(new Date().getTime() + 10000000));
+    	phase.setCreated(new Date());
+    	phase.setUpdated(new Date());
+    	phase.setAuthorId(10144L);
+    	phase.setContestPhaseType(1L);
+    	phase.setContestPK(contest.getContestPK());
+    	
+    	ContestPhaseLocalServiceUtil.addContestPhase(phase);
+    	
+    	
         editedContest = new ContestWrapper(contest);
     	
     }
